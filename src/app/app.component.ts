1b070410a57a532c5128fada280457ca71d23aa0
kain <kainpro@foxmail.com>
2020-08-15 12:48:01 +0800

    update

---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { NgxHowlerService } from 'ngx-howler';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html'
+})
+export class AppComponent implements OnInit {
+  constructor(
+    private howler: NgxHowlerService
+  ) {
+  }
+
+  ngOnInit() {
+    this.howler.register('dev', {
+      src: ['https://cdn.kainonly.com/sound/ding-dong.mp3'],
+      html5: true
+    }).subscribe(status => {
+      // ok
+    });
+  }
+
+  play() {
+    this.howler.get('dev').play();
+  }
+}
